6f44b58a4b1474861e934e19e8575e4f072305bc
ThiagoRson <67815394+ThiagoRson@users.noreply.github.com>
2020-07-22 15:54:11 -0300

    Add files via upload
    Output: $ 5.00 em taxa.

---
+++ Exercicio.Encapsulamento.1/Exercicio.Encapsulamento.1/ContaBancaria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercicio.Encapsulamento._1
+{
+    class ContaBancaria
+    {
+        public string NomeTitular { get; set; }
+        public int Conta { get; private set; }
+        public double DepositoInicial { get; private set; }
+        public char Inicial { get; set; }
+
+        public ContaBancaria (int _conta, string nome)
+        {
+            this.Conta = _conta;
+            NomeTitular = nome;
+            Depositar(DepositoInicial);
+        }
+        public string NomeTItular
+        {
+            get 
+            {
+                return NomeTitular;
+            }
+            set
+            {
+            }
+        }
+
+        public double Deposito (double dep) { 
+                return DepositoInicial = dep;
+        }
+
+        public void Depositar (double posi)
+        {
+            DepositoInicial += posi;
+        }
+        public void Saque(double saca)
+        {
+            DepositoInicial -= saca + 5;;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta " 
+                + Conta + ", Titular: " 
+                + NomeTItular + ", Saldo: $ " 
+                + DepositoInicial.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio.Encapsulamento.1/Exercicio.Encapsulamento.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Globalization;
+
+
+namespace Exercicio.Encapsulamento._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.Write("Entre o numero da conta: ");
+            int Contaa = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Entre o titular da conta: ");
+
+            string Nomee = Console.ReadLine();
+
+            ContaBancaria CF = new ContaBancaria(Contaa, Nomee) ;
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+
+             CF.Inicial = char.Parse(Console.ReadLine());
+
+            if (CF.Inicial == 's' || CF.Inicial == 'S')
+            {
+                Console.Write("\nEntre o valor de depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                CF.Deposito(deposito);
+            }
+
+
+            Console.WriteLine("\nDados da conta: \n" + CF.ToString());
+
+            Console.Write("\nEntre um valor para deposito: ");
+            double depositar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            CF.Depositar(depositar);
+
+            Console.WriteLine("\nDados da conta atualizados: \n" + CF.ToString());
+
+            Console.Write("\nEntre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            CF.Saque(saque);
+
+            Console.WriteLine("\nDados da conta atualizados: \n" + CF.ToString());
+
+        }
+    }
+}
